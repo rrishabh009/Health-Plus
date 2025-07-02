68ffd6a8b1d04979e2d76f673ee7b1e1f3501015
rrishabh009 <trishabh280@gmail.com>
2025-07-02 23:13:24 +0530

    two

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-cimport React from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
